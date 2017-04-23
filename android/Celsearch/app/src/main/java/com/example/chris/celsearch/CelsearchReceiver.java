08d8880a354b32e3dc295e44f15d57c2aea3503d
Chris Acker <christopheracker1@gmail.com>
2017-04-22 23:41:09 -0500

    Add sms intercept functionality

---
+++ android/Celsearch/app/src/main/java/com/example/chris/celsearch/CelsearchReceiver.java
@@ -0,0 +1,49 @@
+package com.example.chris.celsearch;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class CelsearchReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.v("TAG", "chris debug: We are in onReceive.");
+        /*SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+
+        // check each message
+        for (SmsMessage message : messages) {
+
+            // get the address from which it came
+            String sender = message.getOriginatingAddress();
+
+            // get the text from the message
+            String body = message.getMessageBody();
+
+            if (body.startsWith("CS")) {
+                Log.v("TAG", "chris debug: " + body.substring(3, body.length()));
+            }
+
+            // this is used to keep the message from propogating to other broadcast receivers
+            this.abortBroadcast();*/
+        final Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            final Object[] pdusObj = (Object[]) bundle.get("pdus");
+            String format = bundle.getString("format");
+            if (pdusObj == null) {
+                return;
+            }
+            for (int i = 0; i < pdusObj.length; i++) {
+                final SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                String message = currentMessage.getMessageBody();
+                if (message.startsWith("CS")) {
+                    Log.v("TAG", "chris debug: " + message.substring(3, message.length()));
+                }
+            }
+
+        }
+    }
+}
--- android/Celsearch/app/src/main/java/com/example/chris/celsearch/MainActivity.java
+++ android/Celsearch/app/src/main/java/com/example/chris/celsearch/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.chris.celsearch;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            getPermissionToReadSMS();
+        }
+        // check if we are here
+        Log.v("TAG", "chris debug: We are alive.");
+    }
+
+    private static final int READ_SMS_PERMISSIONS_REQUEST = 1;
+
+    public void getPermissionToReadSMS() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_SMS)) {
+                Toast.makeText(this, "Please allow permission!", Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_SMS},
+                    READ_SMS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_SMS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read SMS permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Read SMS permission denied", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }

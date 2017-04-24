e45281205145cb702887dcc67015f74053ce771d
rounaksalim95 <rounaksalim@gmail.com>
2017-04-23 21:48:41 -0500

    Add Python script and some updates

--- web/celSearch/api/controllers/celSearchController.js
+++ web/celSearch/api/controllers/celSearchController.js
@@ -3,6 +3,9 @@
 var mongoose = require('mongoose'),
 	CelSearch = mongoose.model('CelSearch'); 
 
+// Use python-shell so that we can run query_wikipedia.py script for Wikipedia summary
+var PythonShell = require('python-shell');
+
 // Just testing things out 
 
 exports.answerQuery = function(req, res) {
@@ -10,8 +13,25 @@ exports.answerQuery = function(req, res) {
 
 	// Find the document and update it otherwise create it 
 	CelSearch.findOneAndUpdate(req.params.number, req.body, options, function(err, cel) {
-		if (err) 
+		if (err) {
+			console.log('Error');
 			res.send(err);
+		}
+			
+		// Setup the options for PythonShell call
+		var options = {
+			mode: 'text',
+			scriptPath: './api/scripts',
+			args: ['Obama']
+		};
+
+		PythonShell.run('query_wikipedia.py', options, function(err, results) {
+			if (err) {
+				console.log('ERror');
+				throw err;
+			}
+			console.log(results);
+		})
 		res.json(cel);
 	});
 };
